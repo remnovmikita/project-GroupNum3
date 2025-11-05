f7b144d35b8f32a52221af029fbf2995afcad940
Nikita <remnovmikita@gmail.com>
2025-11-05 22:44:54 +0200

    add success-sub

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,14 @@
-/* window.alert("Hello"); */
+const OpenBtnMenu = document.querySelector('.subscribe-btn');
+const modalMenu = document.querySelector('.modal');
+const CloseMdMenu = document.querySelector('.success-btn-close');
+const CloseMdMenutwo = document.querySelector('.modal-close-btn');
+
+OpenBtnMenu.addEventListener('click', () => {
+    modalMenu.classList.add('is-open');
+});
+CloseMdMenu.addEventListener('click', () => {
+    modalMenu.classList.remove('is-open');
+});
+CloseMdMenutwo.addEventListener('click', () => {
+    modalMenu.classList.remove('is-open');
+});
