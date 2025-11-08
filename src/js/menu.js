e345b63faea9d599fa8baa6bf478663f5c734910
Nikita <remnovmikita@gmail.com>
2025-11-08 19:38:18 +0200

    add fixS

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,26 +1,16 @@
-document.getElementById("formEmail").addEventListener("submit", function (x) {
-    
-    const modalMenu = document.querySelector('.modal');
-    const CloseMdMenu = document.querySelector('.success-btn-close');
-    const CloseMdMenutwo = document.querySelector('.modal-close-btn');
-    const userEmail = document.getElementById("userEmail");
-    const erorEmail = document.getElementById("erorEmail");
+document.getElementById("formEmail").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    CloseMdMenu.addEventListener('click', () => {
-    modalMenu.classList.remove('is-open');
-});
-    CloseMdMenutwo.addEventListener('click', () => {
-    modalMenu.classList.remove('is-open');
-});
+  const modalMenu = document.querySelector('.modal');
+  const CloseMdMenu = document.querySelector('.success-btn-close');
+  const CloseMdMenutwo = document.querySelector('.modal-close-btn');
 
-  const emailValue = userEmail.value.trim();
-  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
-    if (!isValidEmail) {
-    x.preventDefault();
-    erorEmail.style.display = "inline"; // показать span
-    
-  } else {
-    erorEmail.style.display = "none"; // скрыть span
-    modalMenu.classList.add('is-open');
-  }
+  modalMenu.classList.add("is-open");
+
+  CloseMdMenu.addEventListener("click", () => {
+    modalMenu.classList.remove("is-open");
+  });
+  CloseMdMenutwo.addEventListener("click", () => {
+    modalMenu.classList.remove("is-open");
+  });
 });
