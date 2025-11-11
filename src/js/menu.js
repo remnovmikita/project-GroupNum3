6d4811beba139b9b35f4225245e3fbf8e3ec4fde
Nikita <remnovmikita@gmail.com>
2025-11-11 19:49:19 +0200

    fixLead

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,11 @@
 document.getElementById("formEmail").addEventListener("submit", function (e) {
-  e.preventDefault();
-
+  const form = e.currentTarget;
   const modalMenu = document.querySelector('.modal');
   const CloseMdMenu = document.querySelector('.success-btn-close');
   const CloseMdMenutwo = document.querySelector('.modal-close-btn');
 
   modalMenu.classList.add("is-open");
+  e.preventDefault();
 
   CloseMdMenu.addEventListener("click", () => {
     modalMenu.classList.remove("is-open");
